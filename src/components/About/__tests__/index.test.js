25096270d9cdba99b86e919266773ea615b4a893
a-wiles <98373402+a-wiles@users.noreply.github.com>
2022-06-14 00:03:32 -0400

    Added compnents with index.js for about and nav

---
+++ src/components/About/__tests__/index.test.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import About from '..';
+
+afterEach(cleanup);
+
+describe('About component', () => {
+    //renders About Test
+})
